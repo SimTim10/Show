c8e76f0da804b6d826cb4189bd8f1b617a8ab45c
Simont93 <113181258+Simomt93@users.noreply.github.com>
2022-09-21 13:36:58 +0200

    The shot

---
+++ Show/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Show
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string x = "Simon";
+            Console.WriteLine(x);
+
+            Console.WriteLine("Insert the first number: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("insert the second number");
+            int z = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(y+z);*/
+
+            int num1 = 6;
+            int num2 = 6;
+
+            if (num1 == num2)
+            {
+                Console.WriteLine("The first number equals the second number");
+            }
+            else
+            {
+                Console.WriteLine("The first number doesn't equal the second number");
+            }
+        }
+    }
+}
